baf0bb17793b23f241faff5f3da074af17d823e4
crisav <avilacris77@gmail.com>
2023-04-18 14:15:04 -0300

    first commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+
+export default function Welcome(props){
+    const[count,setCount]=useState(0)
+    const[semaforo,setSemaforo]=useState(false)
+    const {name}=props;
+
+    const contar=()=>{
+        setCount(count+1)
+        setSemaforo(!semaforo)
+        console.log(semaforo)
+    }
+    return(
+        <div>
+            <p>Hola {name}</p>
+            <button onClick={contar}>Sumar +1</button>
+            <h1>El semaforo esta en {semaforo ? 'rojo':'verde'}</h1>
+            <p>{count}</p>
+        </div>
+    )
+}
